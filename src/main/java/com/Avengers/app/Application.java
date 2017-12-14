c034179091d434bf1cee323262c07337ec840a89
roeyy <roeyyaacovi@gmail.com>
2017-12-14 17:57:35 +0200

    Added connection to database

--- src/main/java/com/Avengers/app/Application.java
+++ src/main/java/com/Avengers/app/Application.java
@@ -1,5 +1,8 @@
 package com.Avengers.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,8 +10,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main( String[] args )
     {
         SpringApplication.run(Application.class, args);
+
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            repository.save(new Customer("Magdolena", "Ippen"));
+
+        };
     }
 }
