de8064fae612cc9a1d717bda5a84f9d08b962608
liorippen <lior.ippen7@gmail.com>
2018-01-11 12:50:47 +0200

    added parser

--- src/main/java/com/Avengers/app/Application.java
+++ src/main/java/com/Avengers/app/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -14,7 +17,72 @@ public class Application {
 
     public static void main( String[] args )
     {
-        SpringApplication.run(Application.class, args);
+   //     SpringApplication.run(Application.class, args);
+        String tableName = "features";
+        ArrayList<ArrayList<ArrayList<String>>> attributes = ReadFile.getAttributesSet();
+        String qs = "CREATE TABLE IF NOT EXISTS "+ tableName + "(";
+        ArrayList<ArrayList<String>> line = attributes.get(0);
+        ArrayList<String> pair;
+        for(int i=0; i<line.size(); i++)
+        {
+            pair = line.get(i);
+            if(i == line.size() - 1)
+            {
+                qs += pair.get(0) + " varchar(2000)" ;
+                break;
+
+            }
+            qs += pair.get(0) + " varchar(225)," ;
+
+        }
+        qs += ")";
+        Statement st = null;
+        int rs ;
+        String q1,q2;
+
+
+        try{
+            Class.forName("org.postgresql.Driver");
+            System.out.println("connect");
+        } catch (ClassNotFoundException cnfe){
+            System.out.println("Could not find the JDBC driver!");
+            System.exit(1);
+        }
+        Connection conn = null;
+        try {
+            //database location, database user, database password
+            conn = DriverManager.getConnection
+                    ("jdbc:postgresql://localhost:5432/testdb","postgres", "xsw21qaz");
+            st = conn.createStatement();
+            rs = st.executeUpdate(qs);
+            for (int j=0; j<attributes.size(); j+=2)
+            {
+                line = attributes.get(j);
+                q1 = "INSERT INTO " + tableName + "(";
+                q2 = " VALUES (";
+                for(int i=0; i<line.size(); i++)
+                {
+                    pair = line.get(i);
+                    if(i == line.size() - 1)
+                    {
+                        q1 += pair.get(0);
+                        q2 += "'" + pair.get(1).substring(1, pair.get(1).length() - 1).replaceAll("'","") + "'";
+                        break;
+
+                    }
+                    q1 += pair.get(0) + ",";
+                    q2 += "'" + pair.get(1).substring(1, pair.get(1).length() - 1).replaceAll("'","") + "'" + ",";
+                }
+                st = conn.createStatement();
+                q1+=")";
+                q2+=")";
+                q1 += q2;
+                rs = st.executeUpdate(q1);
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Could not connect");
+            System.exit(1);
+        }
 
     }
 
@@ -27,3 +95,5 @@ public class Application {
         };
     }
 }
+
+
---
+++ src/main/java/com/Avengers/app/ReadFile.java
@@ -0,0 +1,170 @@
+package com.Avengers.app;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+
+    private static final String FILENAME = "C:\\Users\\uzer1\\Desktop\\small_data_sample.txt";
+
+    public static ArrayList<ArrayList<String>> getAttributeLogMessage(String msg){
+        ArrayList<ArrayList<String>> line= new ArrayList<>();
+        String[] msgAfterSplit = msg.split("message_type");
+        String msgBody = msgAfterSplit[0].split("message:")[1];
+        String afterMsgType = "message_type" + msgAfterSplit[1];
+        msgAfterSplit = afterMsgType.split("\\s");
+        for (String splitBySpace : msgAfterSplit) {
+            ArrayList<String> pair = new ArrayList<>();
+            if (splitBySpace.contains(":") && !splitBySpace.equals(":")) {
+                String[] splitByTowDots = splitBySpace.split(":");
+                if (splitByTowDots[0].equals("timestamp"))
+                {
+                    pair.add("msgtimestamp");
+                }
+                else {
+                    pair.add(splitByTowDots[0]);
+                }
+                pair.add(splitByTowDots[1]);
+                //   int att_inx = attributes.indexOf(splitByTowDots[0]);
+                line.add(pair);
+                //   values.set(att_inx, values.get(attributes.indexOf(splitByTowDots[0])) + 1);
+//                        values.indexOf(values.get(attributes.indexOf(splitByTowDots[0])))
+            }
+        }
+        ArrayList<String> msgPair = new ArrayList<>();
+        msgPair.add("message");
+        msgPair.add(msgBody);
+        line.add(msgPair);
+        return line;
+
+    }
+
+    public static ArrayList<ArrayList<ArrayList<String>>> getAttributesSet() {
+        BufferedReader br = null;
+        FileReader fr = null;
+        ArrayList<ArrayList<ArrayList<String>>> allInfo = new ArrayList<>();
+        List<Integer> values = new ArrayList<Integer>();
+        try {
+            //br = new BufferedReader(new FileReader(FILENAME));
+            fr = new FileReader(FILENAME);
+            br = new BufferedReader(fr);
+
+            String sCurrentLine;
+            int lineNumber = 0;
+            while ((sCurrentLine = br.readLine()) != null) {
+//                boolean isOpen = false;
+                if (lineNumber == 0 || lineNumber == 1) {
+                    lineNumber++;
+                    continue;
+                }
+                ArrayList<ArrayList<String>> line = new ArrayList<>();
+                String[] splitBySpaces = sCurrentLine.split("\\s");
+                for (String splitBySpace : splitBySpaces) {
+                        ArrayList<String> pair = new ArrayList<>();
+                        if (splitBySpace.contains(":") && !splitBySpace.equals(":")) {
+                            String[] splitByTowDots = splitBySpace.split(":");
+                            if (splitByTowDots[0].equals("logMessage")) // this is the last attribute
+                            {
+                                String splitByLogMessage = sCurrentLine.split("logMessage:")[1];
+                                line.addAll(getAttributeLogMessage(splitByLogMessage));
+                                break;
+                            }
+                            else
+                            {
+                                pair.add(splitByTowDots[0]);
+                                pair.add(splitByTowDots[1]);
+                            }
+                         //   int att_inx = attributes.indexOf(splitByTowDots[0]);
+                            line.add(pair);
+                         //   values.set(att_inx, values.get(attributes.indexOf(splitByTowDots[0])) + 1);
+//                        values.indexOf(values.get(attributes.indexOf(splitByTowDots[0])))
+                        }
+                }
+                lineNumber++;
+                allInfo.add(line);
+            }
+//            for(String stt : attributes)
+//            {
+//                System.out.println(stt);
+//            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+                if (fr != null)
+                    fr.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return allInfo;
+    }
+
+//    public static List getValues() {
+//        BufferedReader br = null;
+//        FileReader fr = null;
+//        List<String> values = new ArrayList<String>();
+//        try {
+//            //br = new BufferedReader(new FileReader(FILENAME));
+//            fr = new FileReader(FILENAME);
+//            br = new BufferedReader(fr);
+//
+//            String sCurrentLine;
+//            int lineNumber = 0;
+//            while ((sCurrentLine = br.readLine()) != null) {
+//                if (lineNumber == 0 || lineNumber == 1) {
+//                    lineNumber++;
+//                    continue;
+//                }
+//                String[] splitBySpaces = sCurrentLine.split("\\s");
+//                for (String splitBySpace : splitBySpaces) {
+//                    if (splitBySpace.contains(":") && !splitBySpace.equals(":")) {
+//                        String[] splitByTowDots = splitBySpace.split(":");
+//
+//                        if(!values.contains(splitByTowDots[0]))
+//                        {
+//                            values.add(splitByTowDots[0]);
+//                        }
+//                    }
+//                }
+//                lineNumber++;
+//            }
+////            for(String stt : attributes)
+////            {
+////                System.out.println(stt);
+////            }
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//
+//        } finally {
+//            try {
+//                if (br != null)
+//                    br.close();
+//                if (fr != null)
+//                    fr.close();
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//        return attributes;
+//    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<ArrayList<String>>> lines = getAttributesSet();
+        for (ArrayList<ArrayList<String>> line : lines)
+        {
+            for(ArrayList<String> pair: line) {
+                System.out.println(pair.get(0) + " " + pair.get(1));
+            }
+        }
+    }
+}
