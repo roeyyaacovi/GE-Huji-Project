097152a494d7936b2a2f98634cc551f37056468b
liorippen <lior.ippen7@gmail.com>
2018-04-17 17:21:33 +0300

    final

--- src/main/java/com/Avengers/app/Interface_Module.java
+++ src/main/java/com/Avengers/app/Interface_Module.java
@@ -3,31 +3,14 @@ package com.Avengers.app;
 import java.util.ArrayList;
 import java.util.Map;
 
-class Interface_Module extends Thread {
+abstract class Interface_Module extends Thread {
 
-    private Thread t;
-    private String moduleName;
-    private Framework_Module sync_to;
+    protected Thread t;
+    protected String moduleName;
+    protected Framework_Module sync_to;
 
-    Interface_Module() {}
-    public Interface_Module(String m, Framework_Module s)
-    {
-        moduleName = m;
-        sync_to = s;
-    }
+    abstract public void run();
 
-    public void run()
-    {
-        ArrayList<Map<String, String>> p = new ArrayList<>();
-        sync_to.getData(moduleName, p, 1);
-        System.out.println(moduleName);
-        System.out.println(p.get(0));
-        sync_to.getData(moduleName, p, 1);
-        System.out.println(moduleName);
-        System.out.println(p.get(1));
-        sync_to.alert(moduleName);
-
-    }
 
     public void start() {
         if (t == null) {
