3a6032c1b5af1c6b39d3ec7beba2ab22f0ec7883
liorippen <lior.ippen7@gmail.com>
2018-04-23 00:50:35 +0300

    updated

---
+++ src/main/java/com/Avengers/app/Interface_Module.java
@@ -0,0 +1,22 @@
+package com.Avengers.app;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+abstract class Interface_Module extends Thread {
+
+    protected Thread t;
+    protected String moduleName;
+    protected Framework_Module sync_to;
+
+    abstract public void run();
+
+
+    public void start() {
+        if (t == null) {
+            t = new Thread(this, moduleName);
+            t.start();
+        }
+    }
+
+}
