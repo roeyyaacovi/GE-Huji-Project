86f4a7ed97142e52ad937c8cf795accfe5af3a27
liorippen <lior.ippen7@gmail.com>
2018-05-12 18:29:31 +0300

    some updates

--- src/main/java/com/Avengers/app/UserInterface/UI_Controller.java
+++ src/main/java/com/Avengers/app/UserInterface/UI_Controller.java
@@ -1,9 +1,12 @@
 package com.Avengers.app.UserInterface;
 
+import com.Avengers.app.Framework.Module_Alert;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 
 @RestController
@@ -13,13 +16,14 @@ public class UI_Controller {
     private static final String ATTRIBUTE_2 = "msg";
 
     @GetMapping("/status")
-    private String status(Model model) {
-        int i = 1;
-        for (String module: UI_Module.modules_alerts.keySet()) {
-            model.addAttribute(ATTRIBUTE_1 + Integer.toString(i), module);
-            model.addAttribute(ATTRIBUTE_2 + Integer.toString(i), UI_Module.modules_alerts.get(module));
-            i++;
-        }
-        return "status";
+    private Module_Alert status(Model model) {
+//        int i = 1;
+//        for (String module: UI_Module.modules_alerts.keySet()) {
+//            model.addAttribute(ATTRIBUTE_1 + Integer.toString(i), module);
+//            model.addAttribute(ATTRIBUTE_2 + Integer.toString(i), UI_Module.modules_alerts.get(module));
+//            i++;
+//        }
+//        return "status";
+        return UI_Module.modules_alerts.get("test_module.test_module").getFirst();
     }
 }
